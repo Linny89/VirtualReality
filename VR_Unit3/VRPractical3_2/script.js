f6084ab7b148f51b282951022907afd8fa9b9fa6
Linny89 <lindsayn5@nycstudents.net>
2025-12-01 17:19:59 +0000

    Fine

--- VR_Unit3/VRPractical3_2/script.js
+++ VR_Unit3/VRPractical3_2/script.js
@@ -8,7 +8,13 @@ window.addEventListener("DOMContentLoaded",function() {
       
   loop();
 })
+if(this.flag){
+  this.r += this.DragEvent;
+  this.Object.setAttribute("rotation", {x:5; y:this.DragEvent; z:-1});
+  this.Object.Object3D.rotation.y=this.DragEvent;
+  CaretPosition.y = this.y;
 
+}
 function loop(){
 
     
